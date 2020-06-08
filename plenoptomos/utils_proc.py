691efadecd1fe517bfd037569aa91e25b3a6d1d2
Nicola Vigano <nicola.vigano@esrf.fr>
2020-06-08 12:54:21 +0200

    More linting
    Signed-off-by: Nicola Vigano <nicola.vigano@esrf.fr>

--- plenoptomos/utils_proc.py
+++ plenoptomos/utils_proc.py
@@ -6,14 +6,13 @@ Created on Wed Sep 18 17:00:50 2019
 """
 
 import numpy as np
-import scipy as sp
 import scipy.ndimage as spimg
 import scipy.signal as sps
 
 import matplotlib.pyplot as plt
 # Do not remove the following import: it is used somehow by the plotting
 # functionality in the PSF creation
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D  # noqa: F401
 
 
 def get_smoothing_filter(
@@ -54,15 +53,13 @@ def get_smoothing_filter(
 
 
 def remove_background(
-        data, blur_size, blur_func='circ', axes=(0, 1), do_reverse=False,
-        non_negative=False):
+        data, blur_size, blur_func='circ', axes=(0, 1), do_reverse=False, non_negative=False):
     """
     """
     blur_size = np.array(blur_size, dtype=np.int)
     if blur_size.size == 1:
         blur_size = np.array([blur_size] * 2, dtype=np.int)
-    blur_win = get_smoothing_filter(
-            window_size=blur_size, window_shape=blur_func)
+    blur_win = get_smoothing_filter(window_size=blur_size, window_shape=blur_func)
 
     window_shape = np.ones(len(data.shape), dtype=np.int)
     window_shape[axes[0]], window_shape[axes[1]] = blur_size[0], blur_size[1]
