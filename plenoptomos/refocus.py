b85c25d4b1d125ac1224f4fe0d109e6f182065e6
Nicola VIGANO <nicola.vigano@esrf.fr>
2021-08-19 18:27:43 +0200

    Refocus: fixed docstring and reformatted with black
    Signed-off-by: Nicola VIGANO <nicola.vigano@esrf.fr>

--- plenoptomos/refocus.py
+++ plenoptomos/refocus.py
@@ -43,23 +43,41 @@ def compute_refocus_integration(
     [1] R. Ng, et al., “Light Field Photography with a Hand-held Plenoptic Camera,”
     Stanford Univ. Tech. Rep. CSTR 2005-02, 2005.
 
-    :param lf: The light-field object (lightfield.Lightfield)
-    :param zs: Refocusing distances (numpy.array_like)
-    :param up_sampling: Integer greater than 1 for up-sampling of the final images (int, default: 1)
-    :param border: Number of pixels to extend the border and reduce darkening of edges (int, default: 4)
-    :param border_padding: Border padding method (string, default: 'edge')
-    :param beam_geometry: Beam geometry. Possible options: 'parallel' | 'cone' (string, default: 'parallel')
-    :param domain: Refocusing domain. Possible options: 'object' | 'image' (string, default: 'object')
+    Parameters
+    ----------
+    lf : lightfield.Lightfield
+        The light-field object.
+    zs : numpy.array_like
+        Refocusing distances.
+    beam_geometry : str, optional
+        Beam geometry. Possible options: 'parallel' | 'cone'. The default is "parallel".
+    domain : str, optional
+        Refocusing domain. Possible options: 'object' | 'image'. The default is "object".
+    up_sampling : int, optional
+        Integer greater than 1 for up-sampling of the final images. The default is 1.
+    border : int, optional
+        Number of pixels to extend the border and reduce darkening of edges. The default is 4.
+    border_padding : str, optional
+        Border padding method. The default is "edge".
+    use_multithreading : bool, optional
+        Whether to use multi-threading capabilities. The default is True.
 
-    :returns: Stack of 2D refocused images.
-    :rtype: numpy.array_like
+    Raises
+    ------
+    ValueError
+        In case of wrong domain and beam geometry choices.
+
+    Returns
+    -------
+    imgs : numpy.array_like
+        Stack of 2D refocused images.
     """
     zs = np.array(zs)
     if len(zs.shape) == 0:
         zs = np.expand_dims(zs, 0)
-    if domain.lower() == 'object':
+    if domain.lower() == "object":
         ref_z = lf.camera.get_focused_distance()
-    elif domain.lower() == 'image':
+    elif domain.lower() == "image":
         ref_z = lf.camera.z1
     else:
         raise ValueError('No known domain "%s"' % domain)
@@ -68,8 +86,10 @@ def compute_refocus_integration(
 
     c_in = tm.time()
     m = lf.camera.get_focused_distance() / lf.camera.z1
-    print('Refocusing through Integration %d alphas:\n- Domain = "%s", Beam geometry = "%s", Magnification = %g: ' % (
-        num_alphas, domain, beam_geometry, m), end='', flush=True)
+    print(
+        'Refocusing through Integration %d alphas:\n- Domain = "%s", Beam geometry = "%s", Magnification = %g:'
+        % (num_alphas, domain, beam_geometry, m)
+    )
 
     # Refocusing operate on sub-aperture images:
     lf_sa = lf.clone()
@@ -84,24 +104,25 @@ def compute_refocus_integration(
         renorm_sa_images = np.prod(lf_sa.camera.data_size_vu)
     else:
         lf_sa.data *= lf_sa.flat
-        renorm_sa_images = lf_sa.get_photograph(image='flat') * np.prod(lf_sa.camera.data_size_vu)
+        renorm_sa_images = lf_sa.get_photograph(image="flat") * np.prod(lf_sa.camera.data_size_vu)
 
     camera_sheared = lf_sa.camera.clone()
     if up_sampling > 1:
         camera_sheared.regrid(regrid_size=(1, 1, up_sampling, up_sampling))
         if lf_sa.flat is not None:
-            tt = np.linspace(0.5, lf_sa.camera.data_size_ts[0]-0.5, lf_sa.camera.data_size_ts[0])
-            ss = np.linspace(0.5, lf_sa.camera.data_size_ts[1]-0.5, lf_sa.camera.data_size_ts[1])
+            tt = np.linspace(0.5, lf_sa.camera.data_size_ts[0] - 0.5, lf_sa.camera.data_size_ts[0])
+            ss = np.linspace(0.5, lf_sa.camera.data_size_ts[1] - 0.5, lf_sa.camera.data_size_ts[1])
 
             interp_renorm = sp.interpolate.interp2d(
-                tt, ss, renorm_sa_images, bounds_error=False, fill_value=np.mean(renorm_sa_images))
+                tt, ss, renorm_sa_images, bounds_error=False, fill_value=np.mean(renorm_sa_images)
+            )
 
             tt = np.linspace(
-                0.5 / up_sampling, lf_sa.camera.data_size_ts[0] - 0.5 / up_sampling,
-                lf_sa.camera.data_size_ts[0] * up_sampling)
+                0.5 / up_sampling, lf_sa.camera.data_size_ts[0] - 0.5 / up_sampling, lf_sa.camera.data_size_ts[0] * up_sampling
+            )
             ss = np.linspace(
-                0.5 / up_sampling, lf_sa.camera.data_size_ts[1] - 0.5 / up_sampling,
-                lf_sa.camera.data_size_ts[1] * up_sampling)
+                0.5 / up_sampling, lf_sa.camera.data_size_ts[1] - 0.5 / up_sampling, lf_sa.camera.data_size_ts[1] * up_sampling
+            )
 
             renorm_sa_images = interp_renorm(tt, ss)
 
@@ -113,9 +134,10 @@ def compute_refocus_integration(
 
     imgs = np.empty(imgs_size, lf_sa.data.dtype)
 
-    (samp_v, samp_u, samp_t, samp_s) = lf_sa.camera.get_grid_points(space='direct', domain=domain)
+    (samp_v, samp_u, samp_t, samp_s) = lf_sa.camera.get_grid_points(space="direct", domain=domain)
     interp_lf4D = sp.interpolate.RegularGridInterpolator(
-        (samp_v, samp_u, samp_t, samp_s), lf_sa.data, bounds_error=False, fill_value=0)
+        (samp_v, samp_u, samp_t, samp_s), lf_sa.data, bounds_error=False, fill_value=0
+    )
 
     # Computing the changes of base, and integrating (for each alpha):
     for ii in range(num_alphas):
@@ -154,34 +176,63 @@ def compute_refocus_integration(
 
 
 def compute_refocus_fourier(
-        lf: lightfield.Lightfield, zs, method='slice', beam_geometry='parallel', domain='object',
-        up_sampling=1, border=4, border_padding='edge', padding_factor=1.1, oversampling=2):
-    """Perform the refocusing of the input light field image using Fourier slice theorem method.
+    lf: lightfield.Lightfield,
+    zs,
+    method: str = "slice",
+    beam_geometry: str = "parallel",
+    domain: str = "object",
+    up_sampling: int = 1,
+    border: int = 4,
+    border_padding: str = "edge",
+    padding_factor: float = 1.1,
+    oversampling: float = 2,
+):
+    """
+    Perform the refocusing of the input light field image using Fourier slice theorem method.
 
     This method was presented in:
     [1] R. Ng, “Fourier slice photography,” ACM Trans. Graph., vol. 24, no. 3, p. 735, 2005.
 
-    :param lf: The light-field object (lightfield.Lightfield)
-    :param zs: Refocusing distances (numpy.array_like)
-    :param up_sampling: Integer greater than 1 for up-sampling of the final images (int, default: 1)
-    :param border: Number of pixels to extend the border and reduce darkening of edges (int, default: 4)
-    :param border_padding: Border padding method (string, default: 'edge')
-    :param beam_geometry: Beam geometry. Possible options: 'parallel' | 'cone' (string, default: 'parallel')
-    :param domain: Refocusing domain. Possible options: 'object' | 'image' (string, default: 'object')
-    :param method: Fourier space operation method. Options: 'slice' | 'full', 'hyper_cone' (string, default: 'slice')
-    :param padding: Real-space padding factor (float, default: 1.1)
-    :param oversampling: Fourier space oversampling factor (float, default: 2)
+    Parameters
+    ----------
+    lf : lightfield.Lightfield
+        The light-field object.
+    zs : numpy.array_like
+        Refocusing distances.
+    method : str, optional
+        Fourier space operation method. Options: 'slice' | 'full', 'hyper_cone'. The default is "slice".
+    beam_geometry : str, optional
+        Beam geometry. Possible options: 'parallel' | 'cone'. The default is "parallel".
+    domain : str, optional
+        Refocusing domain. Possible options: 'object' | 'image'. The default is "object".
+    up_sampling : int, optional
+        Integer greater than 1 for up-sampling of the final images. The default is 1.
+    border : int, optional
+        Number of pixels to extend the border and reduce darkening of edges. The default is 4.
+    border_padding : str, optional
+        Border padding method. The default is "edge".
+    padding_factor : float, optional
+        Real-space padding factor. The default is 1.1.
+    oversampling : float, optional
+        Fourier space oversampling factor. The default is 2.
 
-    :returns: Stack of 2D refocused images.
-    :rtype: numpy.array_like
+    Raises
+    ------
+    ValueError
+        In case of wrong domain and beam geometry choices.
+
+    Returns
+    -------
+    imgs : numpy.array_like
+        Stack of 2D refocused images.
     """
 
     zs = np.array(zs)
     if len(zs.shape) == 0:
         zs = np.expand_dims(zs, 0)
-    if domain.lower() == 'object':
+    if domain.lower() == "object":
         ref_z = lf.camera.get_focused_distance()
-    elif domain.lower() == 'image':
+    elif domain.lower() == "image":
         ref_z = lf.camera.z1
     else:
         raise ValueError('No known domain "%s"' % domain)
@@ -189,8 +240,10 @@ def compute_refocus_fourier(
     num_alphas = len(alphas)
 
     m = lf.camera.get_focused_distance() / lf.camera.z1
-    print('Refocusing through Fourier %d alphas:\n- Domain = "%s", Beam geometry = "%s", Magnification = %g, Method = %s:' % (
-        num_alphas, domain, beam_geometry, m, method))
+    print(
+        'Refocusing through Fourier %d alphas:\n- Domain = "%s", Beam geometry = "%s", Magnification = %g, Method = %s:'
+        % (num_alphas, domain, beam_geometry, m, method)
+    )
     c_in = tm.time()
 
     lf_sa = lf.clone()
@@ -205,13 +258,16 @@ def compute_refocus_fourier(
         renorm_sa_images = np.prod(lf.camera.data_size_vu)
     else:
         lf_sa.data *= lf_sa.flat
-        renorm_sa_images = lf.get_photograph(image='flat') * np.prod(lf.camera.data_size_vu)
+        renorm_sa_images = lf.get_photograph(image="flat") * np.prod(lf.camera.data_size_vu)
 
     paddings_ts = (np.ceil(np.array(lf_sa.camera.data_size_ts) * (padding_factor - 1) / 4) * 2).astype(np.int32)
     paddings_vu = np.ceil(np.array(lf_sa.camera.data_size_vu) / 2).astype(np.int32)
-    print("- Padding, border = %d, (v, u, t, s) = (%d, %d, %d, %d).." % (
-        border, paddings_vu[0], paddings_vu[1], paddings_ts[0], paddings_ts[1]),
-        end='', flush=True)
+    print(
+        "- Padding, border = %d, (v, u, t, s) = (%d, %d, %d, %d).."
+        % (border, paddings_vu[0], paddings_vu[1], paddings_ts[0], paddings_ts[1]),
+        end="",
+        flush=True,
+    )
     # Pad image:
     lf_sa.pad((0, 0, border, border), method=border_padding)
     lf_sa.pad((paddings_vu[0], paddings_vu[1], paddings_ts[0], paddings_ts[1]))
@@ -219,20 +275,20 @@ def compute_refocus_fourier(
     data_fft4 = lf_sa.data
 
     c_pad = tm.time()
-    print("\b\b: Done in %g seconds.\n- Performing 4D FFT, dimensions: " % (c_pad - c_in), end='', flush=True)
+    print("\b\b: Done in %g seconds.\n- Performing 4D FFT, dimensions: " % (c_pad - c_in), end="", flush=True)
     # Computing the 4D Fourier transform:
     data_fft4 = fft.ifftshift(data_fft4)
     for ii in range(4):
-        prnt_str = "%d/%d" % (ii+1, 4)
-        print(prnt_str, end='', flush=True)
-        data_fft4 = fft.fftn(data_fft4, axes=(ii, ))
-        print(('\b') * len(prnt_str), end='', flush=True)
+        prnt_str = "%d/%d" % (ii + 1, 4)
+        print(prnt_str, end="", flush=True)
+        data_fft4 = fft.fftn(data_fft4, axes=(ii,))
+        print(("\b") * len(prnt_str), end="", flush=True)
     data_fft4 = fft.fftshift(data_fft4)
 
     c_fft = tm.time()
-    print("\b\b: Done in %g seconds.\n- Creating interpolation grid.." % (c_fft - c_pad), end='', flush=True)
+    print("\b\b: Done in %g seconds.\n- Creating interpolation grid.." % (c_fft - c_pad), end="", flush=True)
 
-    base_grid = lf_sa.camera.get_grid_points(space='fourier', domain=domain)
+    base_grid = lf_sa.camera.get_grid_points(space="fourier", domain=domain)
     interp_lf4D = sp.interpolate.RegularGridInterpolator(base_grid, data_fft4, bounds_error=False, fill_value=0)
 
     camera_sheared = lf_sa.camera.clone()
@@ -245,52 +301,54 @@ def compute_refocus_fourier(
     corr = np.empty(imgs_size, data_fft4.dtype)
 
     c_cre = tm.time()
-    print("\b\b: Done in %g seconds.\n- Performing 2D interpolation: " % (c_cre - c_fft), end='', flush=True)
+    print("\b\b: Done in %g seconds.\n- Performing 2D interpolation: " % (c_cre - c_fft), end="", flush=True)
 
-    (scale_t, scale_s, scale_v, scale_u) = np.array(lf_sa.camera.get_scales(space='fourier', domain=domain))
+    (scale_t, scale_s, scale_v, scale_u) = np.array(lf_sa.camera.get_scales(space="fourier", domain=domain))
     scales = np.array((scale_v, scale_u, scale_t, scale_s))[None, None, :]
 
     # Computing the changes of base, and integrating (for each alpha):
-    if method.lower() == 'slice':
+    if method.lower() == "slice":
         for ii in range(num_alphas):
-            prnt_str = "%d/%d (alpha = %g, z = %g)" % (ii+1, num_alphas, alphas[ii], zs[ii])
-            print(prnt_str, end='', flush=True)
+            prnt_str = "%d/%d (alpha = %g, z = %g)" % (ii + 1, num_alphas, alphas[ii], zs[ii])
+            print(prnt_str, end="", flush=True)
 
             # Change of base:
             sheared_coords = camera_sheared.get_sheared_coords(
-                alphas[ii], space='fourier_slice', beam_geometry=beam_geometry, domain=domain, oversampling=oversampling)
+                alphas[ii], space="fourier_slice", beam_geometry=beam_geometry, domain=domain, oversampling=oversampling
+            )
             imgs[ii, ...] = interp_lf4D(sheared_coords)
 
             dists = 1 - np.abs(sheared_coords) / scales
             corr[ii, ...] = np.prod(dists, axis=2) * np.all(dists > 0, axis=2)
 
-            print(('\b') * len(prnt_str), end='', flush=True)
+            print(("\b") * len(prnt_str), end="", flush=True)
 
-    elif method.lower() in ('full', 'hyper_cone'):
+    elif method.lower() in ("full", "hyper_cone"):
         slice_00 = camera_sheared.get_sheared_coords(
-            np.array((1, )), space='fourier_slice', beam_geometry=beam_geometry, domain=domain, oversampling=oversampling)
+            np.array((1,)), space="fourier_slice", beam_geometry=beam_geometry, domain=domain, oversampling=oversampling
+        )
         for ii in range(num_alphas):
-            prnt_str = "%d/%d (alpha = %g, z = %g)" % (ii+1, num_alphas, alphas[ii], zs[ii])
-            print(prnt_str, end='', flush=True)
+            prnt_str = "%d/%d (alpha = %g, z = %g)" % (ii + 1, num_alphas, alphas[ii], zs[ii])
+            print(prnt_str, end="", flush=True)
 
             # Change of base:
-            if method.lower() == 'full':
+            if method.lower() == "full":
                 sheared_coords = camera_sheared.get_sheared_coords(
-                    alphas[ii], space='fourier', beam_geometry=beam_geometry, domain=domain)
+                    alphas[ii], space="fourier", beam_geometry=beam_geometry, domain=domain
+                )
                 sheared_lf = interp_lf4D(sheared_coords)
-            elif method.lower() == 'hyper_cone':
+            elif method.lower() == "hyper_cone":
                 sheared_lf = data_fft4 * camera_sheared.get_filter(alphas[ii], beam_geometry=beam_geometry, domain=domain)
                 sheared_lf = fft.ifftshift(sheared_lf, axes=(0, 1))
                 sheared_lf = fft.ifftn(sheared_lf, axes=(0, 1))
 
-            interp_obj = sp.interpolate.RegularGridInterpolator(
-                base_grid, sheared_lf, bounds_error=False, fill_value=0)
+            interp_obj = sp.interpolate.RegularGridInterpolator(base_grid, sheared_lf, bounds_error=False, fill_value=0)
             imgs[ii, ...] = interp_obj(slice_00)
 
-            print(('\b') * len(prnt_str), end='', flush=True)
+            print(("\b") * len(prnt_str), end="", flush=True)
 
     c_int = tm.time()
-    print("\b\b: Done in %g seconds.\n- Performing 2D FFT.." % (c_int - c_cre), end='', flush=True)
+    print("\b\b: Done in %g seconds.\n- Performing 2D FFT.." % (c_int - c_cre), end="", flush=True)
     imgs = fft.ifftshift(imgs, axes=(1, 2))
     imgs = fft.ifftn(imgs, axes=(1, 2))
     imgs = np.real(imgs).astype(lf_sa.data.dtype)
@@ -308,13 +366,13 @@ def compute_refocus_fourier(
     if oversampling > 1:
         borders_os = (np.array(imgs.shape[1:]) - camera_sheared.data_size_ts) / 2
         borders_os = np.concatenate((np.ceil(borders_os), np.floor(borders_os))).astype(np.int32)
-        imgs = imgs[:, borders_os[0]:-borders_os[2], borders_os[1]:-borders_os[3]]
+        imgs = imgs[:, borders_os[0] : -borders_os[2], borders_os[1] : -borders_os[3]]
 
     # Crop output images:
     tot_paddings_ts = (paddings_ts + border) * up_sampling
-    imgs = imgs[:, tot_paddings_ts[0]:-tot_paddings_ts[0], tot_paddings_ts[1]:-tot_paddings_ts[1]]
+    imgs = imgs[:, tot_paddings_ts[0] : -tot_paddings_ts[0], tot_paddings_ts[1] : -tot_paddings_ts[1]]
     # Renormalizing:
-    if method.lower() in ('slice', 'full'):
+    if method.lower() in ("slice", "full"):
         imgs *= up_sampling ** 2 / renorm_sa_images
 
     c_out = tm.time()
